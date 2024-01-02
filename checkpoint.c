d1217ebe9f9b4f75481f171b780be489daf97a51
ISMAELSOUSAFONTELES <145488280+ISMAELSOUSAFONTELES@users.noreply.github.com>
2024-01-02 12:32:31 -0300

    Add files via upload

--- checkpoint.c
+++ checkpoint.c
@@ -1,11 +1,4 @@
-/*PROGRESSO DO TRABALHO
 
-1- CRIAMOS A FUNÇÃO QUE DITA A PRIMEIRA JOGADA.
-2- FUNÇÃO JOGO: PARTE QUE DITA AS OPÇÕES DO JOGADOR FEITA
-3- FUNÇÃO COMPRA CONSERTADA.
-4- O JOGO AVISA QUEM GANHOU E FECHA
-5- FALTA SÓ SALVAR E CARREGAR O JOGO
-  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -20,7 +13,7 @@ void compra(int mao[21][2], int mesa[28][2],int contador, int banco){
 }
 
 int Pjogada(int matriz1[21][2],int matriz2[21][2],int mesa[55][2] ,int rodada){
-//Essa função dita qual dos dois jogadores irá começar 
+//Essa função dita qual dos dois jogadores irá começar
   int i, j,indicador1 = 0,indicador2 = 0,soma1=0,soma2=0,maior1[1][2]={{0,0}} ,maior2[1][2],maiorT[1][2];
 
   if(rodada == 1){
@@ -28,7 +21,7 @@ int Pjogada(int matriz1[21][2],int matriz2[21][2],int mesa[55][2] ,int rodada){
       for(j = 0; j < 1; j++){
         //esse loop aqui irá percorrer as mãos dos dois jogadores para verificar as três condições, ou seja, se existe ou não um carrilhão, em caso negativo, qual a peça de maior soma e nos raros casos de empate, irá verificar qual peça possui o maior número
         if(matriz1[i][j] == matriz1[i][j+1]){
-          if((maior1[0][0] = 0) && (maior1[0][1]=0)){
+          if((maior1[0][0] = 0) && (maior1[0][1]=0) ){
             maior1[0][0] = matriz1[i][j];
             maior1[0][1] = matriz1[i][j+1];
             indicador1 = 1;
@@ -85,7 +78,7 @@ int Pjogada(int matriz1[21][2],int matriz2[21][2],int mesa[55][2] ,int rodada){
           }
         }
       }
-    } 
+    }
 
   //Essa parte verifica as condições antes citadas
     if((indicador1 = 1) && (indicador2 = 0)){
@@ -131,7 +124,7 @@ int Pjogada(int matriz1[21][2],int matriz2[21][2],int mesa[55][2] ,int rodada){
       if(maior1[0][0] > maior2[0][0]){
         mesa[27][0] = maior1[0][0];
         mesa[27][1] = maior1[0][1];
-        printf("O jogador 1 começa, a peça [%d/%d] foi jogada para você.\n",mesa[27][0],mesa[27][1]);
+        printf("\nO jogador 1 começa, a peça [%d/%d] foi jogada para você.\n",mesa[27][0],mesa[27][1]);
         for(i = 0;i < 7;i++){
           for(j=0;j<1;j++){
             if((maior1[0][0] == matriz1[i][j])&&(maior1[0][1] == matriz1[i][j+1])){
@@ -150,7 +143,7 @@ int Pjogada(int matriz1[21][2],int matriz2[21][2],int mesa[55][2] ,int rodada){
       else if(maior1[0][0] < maior2[0][0]){
         mesa[27][0] = maior2[0][0];
         mesa[27][1] = maior2[0][1];
-        printf("O jogador 2 começa, a peça [%d/%d] foi jogada para você.\n",mesa[27][0],mesa[27][1]);
+        printf("\nO jogador 2 começa, a peça [%d/%d] foi jogada para você.\n",mesa[27][0],mesa[27][1]);
         indicador2 = 2;
         for(i = 0;i < 7;i++){
           for(j=0;j<1;j++){
@@ -301,8 +294,8 @@ int peca_valida(int mao[21][2], int tabuleiro[55][2],int numpecas, int e, int d)
               vetor[j][1] =  mao[i][1];
               j++;
               result = 1;
-            } 
-        } 
+            }
+        }
         if(result == 1){
           printf("\nPEÇAS VALIDAS: \n");
         for (i = 0; i < j; i++)
@@ -313,16 +306,10 @@ int peca_valida(int mao[21][2], int tabuleiro[55][2],int numpecas, int e, int d)
         }
         printf("\n");
         return result;
-} 
-int GANHOU(int mao[21][2]){
-  int i, j, v;
-  if(mao[0][0] == 0 && mao[0][1] == 0){
-    printf("Jogador desta rodada ganhou");
-  }
-  
 }
+
 int JOGO(int matriz[55][2],int mao[21][2], int e, int d)
-{ 
+{
   int peca_jogada[2];
   int decisao;
   int i;
@@ -351,7 +338,7 @@ int JOGO(int matriz[55][2],int mao[21][2], int e, int d)
         for(i = 0; i < 21; i++)
         {
           if((peca_jogada[0] == mao[i][0] && peca_jogada[1] == mao[i][1]) || (peca_jogada[0] == mao[i][1] && peca_jogada[1] == mao[i][0]))
-          {  
+          {
             //usa-se função peça valida(modificada) para conferir se a peça aqui pode ser jogada.
 
             if (peca_jogada[0] == matriz[e][0] ||  peca_jogada[1] == matriz[e][0])
@@ -411,9 +398,10 @@ int JOGO(int matriz[55][2],int mao[21][2], int e, int d)
 int main(){
     int choice,loop = 1,loopint = 1, contador1 = 7, contador2 = 7, vez1 = 0, vez2 = 0, rodada = 1, movimento = 0;
     int pecas_no_banco = 14;
+    FILE *file;
   while(loop == 1){
     printf("Bem vindo à Super Dominó's Online 3d Multiplayer.apk!\n\n");
-    printf("Menu inicial\n\n1-Nova partida\n2-Carregar partida\n3-Regras do Super Dominó's Online 3d Multuplayer.apk!\n4-Sair para a tela inicial\n");
+    printf("Menu inicial\n\n1-Nova partida\n2-Carregar partida\n3-Regras do Super Dominó's Online 3d Multiplayer.apk!\n4-Sair para a tela inicial\n");
     choice = lerInteiro();
 
     if(choice == 1){
@@ -526,11 +514,11 @@ int main(){
            printf("\nTABULEIRO:\n\n");
            for(i = e; i < d + 1; i++)
            {
-               printf(" [ %d|%d ]" , linha[i][0], linha[i][1]);
+             printf(" [ %d|%d ]" , linha[i][0], linha[i][1]);
            }
           printf("\nEscolha seu movimento\n1-Jogar\n2-Comprar uma peça\n3-Passar sua vez\n4-Salvar e sair\n");
           movimento = lerInteiro();
-           
+
           if(movimento == 1){
 
 
@@ -540,7 +528,7 @@ int main(){
 
             n = JOGO(linha, mao1, e, d);
             if(n == 0)
-             { 
+             {
                remover(linha[e-1][0], linha[e-1][1],mao1, contador1);
                contador1 --;
                if(contador1 == 0){
@@ -586,7 +574,7 @@ int main(){
                 }
             }
           }
-          
+
           if(movimento == 3){
             if(peca_valida(mao1,linha,contador1,e,d) != 1 && pecas_no_banco == 0)
             {
@@ -602,10 +590,32 @@ int main(){
             printf("\nTem certeza que deseja Salvar e Sair?\n1-Sim\n2-Não\n");
             escolha = lerInteiro();
             if(escolha == 1){
+              file = fopen("DOMINO.txt", "w");
+              if(file){
+                fprintf(file, "%d\n%d\n", contador1, contador2);
+                for(int i = 0; i < contador1; i++){
+                    fprintf(file, "%d %d ", mao1[i][0], mao1[i][1]);
+                }
+                fprintf(file, "\n");
+                for(int i = 0; i < contador2; i++){
+                    fprintf(file, "%d %d ", mao2[i][0], mao2[i][1]);
+                }
+                fprintf(file, "\n%d\n%d\n%d\n%d\n%d\n", vez1, vez2, rodada, e, d);
+                for(i = e; i < d + 1; i++)
+           {
+             fprintf(file, "%d %d " , linha[i][0], linha[i][1]);
+           }
+                fprintf(file,"\n");
+                for(i = 0; i < 14; i++){
+                   fprintf(file, "%d ", linhas[i]);
+                 }
+                fclose(file);
+                return 0;
+              }
               break;
             }
           }
-          }  
+          }
         }
        }
         if(vez2 == rodada){
@@ -632,7 +642,7 @@ int main(){
            movimento = lerInteiro();
            if(contador2 == 0){
              printf("Jogador 2 venceu");
-             
+
            }
            if(movimento == 1){
 
@@ -706,17 +716,328 @@ int main(){
              printf("Tem certeza que deseja Salvar e Sair?\n1-Sim\n2-Não");
              escolha = lerInteiro();
              if(escolha == 1){
+            file = fopen("DOMINO.txt", "w");
+              if(file){
+                fprintf(file, "%d\n%d\n", contador1, contador2);
+                for(int i = 0; i < contador1; i++){
+                    fprintf(file, "%d %d ", mao1[i][0], mao1[i][1]);
+                }
+                fprintf(file, "\n");
+                for(int i = 0; i < contador2; i++){
+                    fprintf(file, "%d %d ", mao2[i][0], mao2[i][1]);
+                }
+                fprintf(file, "\n%d\n%d\n%d\n%d\n%d\n", vez1, vez2, rodada, e, d);
+                for(i = e; i < d + 1; i++)
+           {
+             fprintf(file, "%d %d " , linha[i][0], linha[i][1]);
+           }
+                fprintf(file,"\n");
+           for(i = 0; i < 14; i++){
+             fprintf(file, "%d ", linhas[i]);
+           }     
+                fclose(file);
+                return 0;
+              }
                break;
              }
            }
          }
-       }   
+       }
       }
      }
     }
     if(choice == 2){
-      printf("\nescolha 2\n");
-    }
+      file = fopen("DOMINO.txt", "r+");
+      int mao1[21][2],mao2[21][2],linha[55][2],i=0,j,a,linhas[14],aux, e=27, d=27, n, z=0;
+          int mesa[28][2]={{0,0},{0,1},{1,1},{2,0},{2,1},{2,2},{3,0},{3,1},{3,2},{3,3},{4,0},{4,1},{4,2},{4,3},{4,4},{5,0},{5,1},{5,2},{5,3},{5,4},{5,5},{6,0},{6,1},{6,2},{6,3},{6,4},{6,5},{6,6}};
+      if(file){
+          fscanf(file, "%d", &contador1 );
+          fscanf(file, "%d", &contador2 );
+          for(int i = 0; i<contador1; i++){
+          fscanf(file, "%d %d", &mao1[i][0], &mao1[i][1]);
+          }
+          for(int i = 0; i<contador2; i++){
+          fscanf(file, "%d%d ", &mao2[i][0], &mao2[i][1]);
+          }
+          fscanf(file, "%d", &vez1 );
+          fscanf(file, "%d", &vez2 );
+          fscanf(file, "%d", &rodada );
+          fscanf(file, "%d", &e );
+          fscanf(file, "%d", &d );
+          for(i = e; i < d + 1; i++)
+           {
+             fscanf(file, "%d %d" , &linha[i][0], &linha[i][1]);
+           }
+          for(i = 0; i < 14; i++){
+            fscanf(file, "%d", &linhas[i]);
+          }
+          fclose(file);
+          }
+      for(i=0;i<14;i++){
+        for(a=0;a<28;a++){
+            if(linhas[i] == a){
+                do{
+                    mesa[a][0] = mesa[a+1][0];
+                    mesa[a][1] = mesa[a+1][1];
+                    a++;
+                }while(a<28);
+            }
+        }
+      }
+          if(vez1>vez2){
+            rodada++;
+            vez1--;
+            vez2++;
+          }
+          else{
+            rodada++;
+            vez1++;
+            vez2--;
+          }
+          while(loop == 1) {
+       if(vez1 == rodada){
+         vez1 = vez1 + 2;
+         while(loopint == 1){
+          if(contador1 == 0){
+            printf("\n\n!!!!!JOGADOR 1 VENCEU!!!!!\n\n");
+            exit(0);
+          }
+          else{
+          printf("\nRODADA %d\n", rodada);
+
+          printf("\nTurno do jogador 1\n");
+          system("read -p \"PRESSIONE ENTER PARA CONTINUAR\" continue ");
+          printf("\n\nSua mão:\n\n");
+           for(int i=0; i <contador1;i++){
+             printf("[%d/%d]",mao1[i][0],mao1[i][1]);
+           }
+           printf("\nTABULEIRO:\n\n");
+           for(i = e; i < d + 1; i++)
+           {
+             printf(" [ %d|%d ]" , linha[i][0], linha[i][1]);
+           }
+          printf("\nEscolha seu movimento\n1-Jogar\n2-Comprar uma peça\n3-Passar sua vez\n4-Salvar e sair\n");
+          movimento = lerInteiro();
+
+          if(movimento == 1){
+
+
+
+            if(peca_valida(mao1,linha,contador1,e,d) == 1){
+            //Função do Xamuel
+
+            n = JOGO(linha, mao1, e, d);
+            if(n == 0)
+             {
+               remover(linha[e-1][0], linha[e-1][1],mao1, contador1);
+               contador1 --;
+               if(contador1 == 0){
+                 printf("Jogador 1 venceu");
+                 exit(0);
+               }
+               e--;
+              rodada ++;
+              break;
+             }
+             else if(n == 1)
+             {
+               remover(linha[d + 1][0], linha[d + 1][1],mao1, contador1);
+                contador1 --;
+               d++;
+              rodada ++;
+              break;
+             }
+            else if(n == 2)
+             {
+              continue;
+             }
+            }
+            else{
+              printf("JOGADA INVALIDA, COMPRE UMA PEÇA!\n");
+            }
+          }
+
+          if(movimento == 2){
+            if(pecas_no_banco == 0){
+              printf("Sem peças disponíveis, jogue ou passe a vez.\n");
+            }
+            else{
+                if(peca_valida(mao1,linha,contador1,e,d) == 1)
+                {
+                  printf("PEÇAS DISPONIVEIS NA MÃO");
+                }
+                else
+                {
+                compra(mao1, mesa, contador1, pecas_no_banco);
+                pecas_no_banco --;
+                contador1++;
+                }
+            }
+          }
+
+          if(movimento == 3){
+            if(peca_valida(mao1,linha,contador1,e,d) != 1 && pecas_no_banco == 0)
+            {
+             printf("Você passou sua vez!");
+             rodada++;
+             break;
+            }else{
+              printf("JOGUE UMA PEÇA OU COMPRE DA MESA");
+            }
+          }
+          if(movimento == 4){
+            int escolha;
+            printf("\nTem certeza que deseja Salvar e Sair?\n1-Sim\n2-Não\n");
+            escolha = lerInteiro();
+            if(escolha == 1){
+              file = fopen("DOMINO.txt", "w");
+              if(file){
+                fprintf(file, "%d\n%d\n", contador1, contador2);
+                for(int i = 0; i < contador1; i++){
+                    fprintf(file, "%d %d ", mao1[i][0], mao1[i][1]);
+                }
+                fprintf(file, "\n");
+                for(int i = 0; i < contador2; i++){
+                    fprintf(file, "%d %d ", mao2[i][0], mao2[i][1]);
+                }
+                fprintf(file, "\n%d\n%d\n%d\n%d\n%d\n", vez1, vez2, rodada, e, d);
+                    for(i = e; i < d + 1; i++)
+           {
+             fprintf(file, "%d %d " , linha[i][0], linha[i][1]);
+           }
+                fclose(file);
+                return 0;
+              }
+              break;
+            }
+          }
+          }
+        }
+       }
+        if(vez2 == rodada){
+          vez2 = vez2 + 2;
+          while(loopint == 1){
+            if(contador2 == 0){
+              printf("\n\n!!!!!JOGADOR 2 VENCEU!!!!!\n\n");
+              exit(0);
+            }
+            else{
+           printf("\nRODADA %d\n", rodada);
+           printf("\nTurno do jogador 2\n");
+            system("read -p \"PRESSIONE ENTER PARA CONTINUAR\" continue ");
+            printf("Sua mão:\n");
+            for(i=0; i <contador2;i++){
+              printf("[%d/%d]",mao2[i][0],mao2[i][1]);
+            }
+            printf("\nTABULEIRO:\n\n");
+             for(i = e; i < d + 1; i++)
+             {
+                 printf(" [ %d|%d ]" , linha[i][0], linha[i][1]);
+             }
+           printf("\nEscolha seu movimento\n1-Jogar\n2-Comprar uma peça\n3-Passar sua vez\n4-Salvar e sair\n");
+           movimento = lerInteiro();
+           if(contador2 == 0){
+             printf("Jogador 2 venceu");
+
+           }
+           if(movimento == 1){
+
+
+
+            if(peca_valida(mao2,linha,contador2,e,d) == 1){
+             //Função do Xamuel
+             n = JOGO(linha, mao2, e, d);
+              if(contador2 == 0){
+                 printf("Jogador 2 venceu!");
+                exit(0);
+              }
+             if(n == 0)
+             {
+               remover(linha[e-1][0], linha[e-1][1],mao2, contador2);
+                contador2 --;
+               e--;
+              rodada ++;
+              break;
+             }
+             else if(n == 1)
+             {
+               remover(linha[d + 1][0], linha[d + 1][1],mao2, contador2);
+                contador2 --;
+               if(contador2 == 0){
+                  printf("Jogador 2 venceu!");
+                 exit(0);
+               }
+               d++;
+              rodada ++;
+              break;
+             }
+             else if(n == 2)
+             {
+              continue;
+             }
+            }
+            else{
+              printf("JOGADA INVALIDA, COMPRE UMA PEÇA!");
+            }
+           }
+           if(movimento == 2){
+              if(pecas_no_banco == 0){
+                printf("Sem peças disponíveis, jogue ou passe a vez.\n");
+              }
+              else{
+                if(peca_valida(mao2,linha,contador2,e,d) == 1)
+                {
+                  printf("PEÇAS DISPONIVEIS NA MÃO");
+                }
+                else
+                {
+                compra(mao2, mesa, contador2, pecas_no_banco);
+                pecas_no_banco --;
+                contador2++;
+                }
+              }
+           }
+           if(movimento == 3){
+             if(peca_valida(mao2,linha,contador2,e,d) != 1 && pecas_no_banco == 0)
+             {
+              printf("Você passou sua vez!");
+              rodada++;
+              break;
+             }else{
+               printf("JOGUE UMA PEÇA OU COMPRE DA MESA");
+             }
+           }
+           if(movimento == 4){
+             int escolha;
+             printf("Tem certeza que deseja Salvar e Sair?\n1-Sim\n2-Não");
+             escolha = lerInteiro();
+             if(escolha == 1){
+            file = fopen("DOMINO.txt", "w");
+              if(file){
+                fprintf(file, "%d\n%d\n", contador1, contador2);
+                for(int i = 0; i < contador1; i++){
+                    fprintf(file, "%d %d ", mao1[i][0], mao1[i][1]);
+                }
+                fprintf(file, "\n");
+                for(int i = 0; i < contador2; i++){
+                    fprintf(file, "%d %d ", mao2[i][0], mao2[i][1]);
+                }
+                fprintf(file, "\n%d\n%d\n%d\n%d\n%d\n", vez1, vez2, rodada, e, d);
+                for(i = e; i < d + 1; i++)
+           {
+             fprintf(file, "%d %d " , linha[i][0], linha[i][1]);
+           }
+                fclose(file);
+                return 0;
+              }
+               break;
+             }
+           }
+         }
+       }
+      }
+     }
+}
     else if(choice == 3){
       printf(" \nREGRAS DO JOGO \n\n 1- O jogo começa com um conjunto de 28 peças únicas e diferentes, variando de [0|0] até [6|6].\n • Cada jogador começa comprando 7 peças. \n • O jogador inicial é o que possui a dupla mais alta. Se nenhum jogador possuir uma dupla, o que tiver a maior soma inicia. Em caso de empate na soma, o jogador com a peça de maior número em sua composição começa.\n • Em cada vez, o jogador deve realizar uma das três ações: jogar uma peça de sua mão, comprar peças da mesa até que seja possível jogar ou passar a vez, se todas as peças da mesa estiverem esgotadas.\n • As regras para jogar uma peça da mão para a mesa são as seguintes: é permitido jogar peças que tenham um lado com o mesmo número do lado esquerdo da peça mais à esquerda jogada ou um lado com o mesmo número do lado direito da peça mais à direita. Se ambas as possibilidades forem o mesmo número, o jogador pode escolher em que lado quer jogar a peça.\n • Após a jogada, a peça jogada deve se conectar à peça que estava em mesa pelo lado com o número em comum, formando assim uma sequência linear de peças.\n ");
     }
